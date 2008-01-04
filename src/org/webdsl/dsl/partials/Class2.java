0ab8cf6e74755266ade60deffd9bb5c07c9944b7
Zef Hemel <zef@zef.me>
2008-01-04 15:55:51 +0000

    Temporary place for partial class/method merger. It seems to work, but constraint checking is very limited currently. It is usable though.
    svn path=/webdsls/trunk/; revision=871

---
+++ src/org/webdsl/dsl/partials/Class2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.io.File;
+
+@Partial
+class TheClass implements Comparable {
+	int a;
+	boolean c;
+
+	@Partial
+	public void doSomething() {
+		System.out.println("Hello another time!");
+	}
+	
+	public void hoi() {
+		
+	}
+}
