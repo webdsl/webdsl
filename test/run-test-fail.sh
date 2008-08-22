7f29cad28e0c7d77e5f3046e9011581aa83d37ad
Lennart Kats <lclkats@gmail.com>
2008-08-22 09:34:10 +0000

    "Vernieuwering"
    svn path=/webdsls/trunk/; revision=1746

--- test/run-test-fail.sh
+++ test/run-test-fail.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
-name=$0.app
-show_result=$1
-dsl-to-seam -i $name --stop-after 1 > /dev/null 2> $name.out
-grep "$(cat $name | head -n 1 | sed 's/^[/]*//')" $name.out > /dev/null
+SRCDIR=$(pwd)/../src
+cd `dirname $0`
+FILE=`basename $0`
+bash $SRCDIR/org/webdsl/dsl/project/webdsl test $FILE > /dev/null 2> $FILE.out
+grep "$(cat $FILE.app | head -n 1 | sed 's/^[/]*//')" $FILE.out > /dev/null
+
 result=$?
-if test 0 -eq $result; then
-  #echo "SUCCEEDED in finding error: $name"
-  rm $name.out
+if test 0 -ne $result; then
+  cat $FILE.out
+  rm -f $FILE.out
+  exit 1
 else
-  if [ "$show_result" == "show" ]; then
-    #echo "FAILED in finding error: $name output:"
-    cat $name.out
-  else
-    :
-    #echo "FAILED in finding error: $name (output in $name.out)"
-  fi
+  rm -f $FILE.out
+  exit 0
 fi
-test 0 -eq $result
