ea29a1ac6563465f4a7d0bb9b1208fe327cc4f60
Zef Hemel <zef@zef.me>
2008-08-27 11:58:15 +0000

    Getting more and more stying to work
    svn path=/webdsls/trunk/; revision=1805

--- src/org/webdsl/dsl/project/template-python/webdsl/utils.py
+++ src/org/webdsl/dsl/project/template-python/webdsl/utils.py
@@ -100,8 +100,9 @@ def register(path, cls, param_mappings=[]):
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>%s</title>
     %s
-    <!--link href="/stylesheets/dropdownmenu.css" rel="stylesheet" type="text/css" />
-    <link href="/stylesheets/sdmenu.css" rel="stylesheet" type="text/css" /-->
+    <link href="/stylesheets/common_.css" rel="stylesheet" type="text/css" />
+    <link href="/stylesheets/dropdownmenu.css" rel="stylesheet" type="text/css" />
+    <link href="/stylesheets/sdmenu.css" rel="stylesheet" type="text/css" />
     <link href="/stylesheets/calendar.css" rel="stylesheet" type="text/css" />
     <script type='text/javascript' src='/javascripts/dropdownmenu.js'></script>
     <script type='text/javascript' src='/javascripts/sdmenu.js'></script>
