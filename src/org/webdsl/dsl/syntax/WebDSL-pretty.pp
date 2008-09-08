f62681fdb57b916e5e2be105b5781bccd95d6af4
Danny Groenewegen <dgroenewegen@gmail.com>
2008-09-08 15:49:19 +0000

    missing pp entries
    svn path=/webdsls/trunk/; revision=1909

--- src/org/webdsl/dsl/syntax/WebDSL-pretty.pp
+++ src/org/webdsl/dsl/syntax/WebDSL-pretty.pp
@@ -125,8 +125,10 @@
 
    ForStmt                            -- V[H[KW["for"] KW["("] _1 KW[":"] _2 KW["in"] _3 _4 KW[")"] ] _5],
    ForStmt.5:iter-star                -- _1,
+   ForStmtNoFilter                    -- V[H[KW["for"] KW["("] _1 KW[":"] _2 KW["in"] _3 KW[")"] ] _4],
    ForAllStmt                         -- V[H[KW["for"] KW["("] _1 KW[":"] _2 _3 KW[")"] ] _4],
    ForAllStmt.4:iter-star             -- _1,
+   ForAllStmtNoFilter                 -- V[H[KW["for"] KW["("] _1 KW[":"] _2 KW[")"] ] _3],
 
    ForExp                             -- H[KW["["] _1 KW["|"] _2 KW[":"] _3 KW["in"] _4 _5 KW["]"]],
    ForExpNoFilter                     -- H[KW["["] _1 KW["|"] _2 KW[":"] _3 KW["in"] _4 KW["]"]],
