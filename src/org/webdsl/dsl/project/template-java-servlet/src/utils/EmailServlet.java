524b5821e89cadf51dfde2ff3e855662a3253777
Danny Groenewegen <dgroenewegen@gmail.com>
2008-11-17 13:20:12 +0000

    class generated for email, reuses render translation for templates. some refactoring, moved type(name) utilities to type-utils.str
    svn path=/webdsls/trunk/; revision=2223

---
+++ src/org/webdsl/dsl/project/template-java-servlet/src/utils/EmailServlet.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public interface EmailServlet {
+
+    public int render(PageServlet ps,Object[] args,int templatecounter,HashMap<String, Class> templates,HashMap<String, Object> variables,HashMap<String, Object> actionclasses, java.io.PrintWriter out) ;
+}
