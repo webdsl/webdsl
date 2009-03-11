d3e8eeb1be781bff7c82842dc5a614df84cd773a
Danny Groenewegen <dgroenewegen@gmail.com>
2009-03-11 09:10:11 +0000

    id property of entities now visible
    created a script that compares the default generated pp table and the existing one, and lists the new entries from the generated pp table
    
    updated pp table with missing entries
    
    svn path=/webdsls/trunk/; revision=2513

---
+++ src/org/webdsl/dsl/syntax/update-pp.sh
@@ -0,0 +1,17 @@
+ #!/bin/sh
+ppgen -i WebDSL.def -o __generated-webdsl-pp.pp
+# parse-pp-table cannot parse this line
+sed '/HqlQuery.1:parameterized-sort.1:"QueryRule"/d' __generated-webdsl-pp.pp > __generated-webdsl-pp-fixed.pp
+parse-pp-table -i __generated-webdsl-pp-fixed.pp -o __generated-webdsl-pp.pp.af
+parse-pp-table -i WebDSL-pretty.pp -o __WebDSL-pretty.pp.af
+pptable-diff -i __generated-webdsl-pp.pp.af --old __WebDSL-pretty.pp.af 2> __ppdiff
+cat __ppdiff
+sed -n '/New entries/p' __ppdiff > __ppdiff_new_entries
+#cat __ppdiff_new_entries
+echo "" > __ppdiff_new_entries_for_insertion
+for i in `cat __ppdiff_new_entries | grep -o \"[^\"]*\" | sed "s:\"::g"` ; do  
+#  echo $i;
+  sed -n '/'$i'/p' __generated-webdsl-pp-fixed.pp >> __ppdiff_new_entries_for_insertion
+done;
+cat __ppdiff_new_entries_for_insertion | uniq > __ppdiff_new_entries_for_insertion2
+cat __ppdiff_new_entries_for_insertion2
