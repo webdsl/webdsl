69ffd4c2284a885a1bf79cdd90fefcdddc98cf6c
Eelco Dolstra <eelco.dolstra@logicblox.com>
2009-03-19 18:03:05 +0000

    * Hydra job expression for WebDSL.  To test from the command-line:
        $ nix-build release.nix -A tarball
    
      to build a tarball, or
      
        $ nix-build release.nix -A build
    
      to do a Nix build.  There are no RPMs etc. yet.
    
    svn path=/webdsls/trunk/; revision=2557

---
+++ release.nix
@@ -0,0 +1,50 @@
+{nixpkgs ? ../nixpkgs}:
+
+let
+
+  pkgs = import nixpkgs {};
+
+
+  jobs = rec {
+
+
+    tarball =
+      { webdslsSrc ? {outPath = pkgs.lib.cleanSource ./.; rev = 1234;}
+      , officialRelease ? false
+      }:
+
+      with pkgs;
+
+      pkgs.releaseTools.makeSourceTarball {
+        name = "webdsl-tarball";
+        src = webdslsSrc;
+        inherit officialRelease;
+        buildInputs = [
+          pkgconfig strategoPackages.aterm strategoPackages.sdf
+          strategoPackages.strategoxt strategoPackages.javafront
+        ];
+      };
+
+
+    build =
+      { tarball ? jobs.tarball {}
+      , system ? "i686-linux"
+      }:
+
+      with import nixpkgs {inherit system;};
+
+      releaseTools.nixBuild {
+        name = "php-front";
+        src = tarball;
+        buildInputs = [
+          pkgconfig strategoPackages.aterm strategoPackages.sdf
+          strategoPackages.strategoxt strategoPackages.javafront
+          apacheAnt
+        ];
+      };
+
+
+  };
+
+  
+in jobs
