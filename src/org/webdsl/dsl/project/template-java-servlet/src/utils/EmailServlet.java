4b53826c21137f297379750a29ca47a47f2fb5c6
Danny Groenewegen <dgroenewegen@gmail.com>
2009-03-26 15:49:19 +0000

    move init to superclass
    svn path=/webdsls/trunk/; revision=2588

--- src/org/webdsl/dsl/project/template-java-servlet/src/utils/EmailServlet.java
+++ src/org/webdsl/dsl/project/template-java-servlet/src/utils/EmailServlet.java
@@ -8,23 +8,37 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public abstract class EmailServlet {
 
-    public abstract void render(PageServlet ps, Object[] args, HashMap<String, Object> variables, java.io.PrintWriter out);
-    
-    public static java.util.Properties props = new java.util.Properties();
-    
-    static {
-      try {    
-        props.load(EmailServlet.class.getResourceAsStream("/email.properties")) ;
-      }
-      catch(java.io.FileNotFoundException fnf) {
-        System.out.println("File \"email.properties\" not found");
-      }
-      catch(IOException io) {
-        System.out.println("IOException while reading \"email.properties\"");   
-      }
-    }
+	public abstract void render(PageServlet ps, Object[] args, HashMap<String, Object> variables, java.io.PrintWriter out);
+
+	protected static java.util.Properties props = new java.util.Properties();
+	protected static java.util.Properties sessionProps = new java.util.Properties();
+	protected static String host;
+	protected static String username;
+	protected static String password;
+
+	protected static javax.mail.Session getSession(){
+		return javax.mail.Session.getInstance(sessionProps, null);
+	}
+
+	static {
+		try {    
+			props.load(EmailServlet.class.getResourceAsStream("/email.properties"));
+			host     = EmailServlet.props.getProperty("webdsl.email.host");
+			username = EmailServlet.props.getProperty("webdsl.email.user");
+			password = EmailServlet.props.getProperty("webdsl.email.pass");
+			sessionProps.put("mail.smtps.auth", "true");
+			sessionProps.put("mail.smtps.port", EmailServlet.props.getProperty("webdsl.email.port"));
+			javax.mail.Session session = javax.mail.Session.getInstance(sessionProps, null);
+			//TODO set SSL/TLS	
+		}
+		catch(java.io.FileNotFoundException fnf) {
+			System.out.println("File \"email.properties\" not found");
+		}
+		catch(IOException io) {
+			System.out.println("IOException while reading \"email.properties\"");   
+		}
+	}
 
 }
