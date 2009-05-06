378c09e843bcdefaadfde8a2748988810009d058
Danny Groenewegen <dgroenewegen@gmail.com>
2009-05-06 13:42:02 +0000

    svn path=/webdsls/trunk/; revision=2665

--- src/org/webdsl/dsl/project/template-java-servlet/src/utils/HTMLFilter.java
+++ src/org/webdsl/dsl/project/template-java-servlet/src/utils/HTMLFilter.java
@@ -1,33 +1,14 @@
-/*
-* Licensed to the Apache Software Foundation (ASF) under one or more
-* contributor license agreements.  See the NOTICE file distributed with
-* this work for additional information regarding copyright ownership.
-* The ASF licenses this file to You under the Apache License, Version 2.0
-* (the "License"); you may not use this file except in compliance with
-* the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
 package utils;
-
-/**
- * HTML filter utility.
- *
- * @author Craig R. McClanahan
- * @author Tim Tye
- * @version $Revision: 467217 $ $Date: 2006-10-24 05:14:34 +0200 (Tue, 24 Oct 2006) $
- */
 
 public final class HTMLFilter {
 
-
     /**
+     * HTML filter utility from tomcat utils with a reverse added.
+	 *
+	 * @author Craig R. McClanahan
+	 * @author Tim Tye
+	 * @version $Revision: 467217 $ $Date: 2006-10-24 05:14:34 +0200 (Tue, 24 Oct 2006) $
+	 * 
      * Filter the specified message string for characters that are sensitive
      * in HTML.  This avoids potential attacks caused by including JavaScript
      * codes in the request URL that is often reported in error messages.
