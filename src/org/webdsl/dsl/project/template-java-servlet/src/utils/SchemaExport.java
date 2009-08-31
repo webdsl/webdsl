eb45077aaa98a3d34c6480b495ff229919cff4a5
Danny Groenewegen <dgroenewegen@gmail.com>
2009-08-31 12:31:30 +0000

    'schema-export' Ant task; writes create script to schema-export.sql
    svn path=/webdsls/trunk/; revision=2978

---
+++ src/org/webdsl/dsl/project/template-java-servlet/src/utils/SchemaExport.java
@@ -0,0 +1,47 @@
+package utils;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
+import org.hibernate.*;
+
+import java.util.*;
+
+public class SchemaExport {
+
+	static Session hibSession;
+	
+	public static void main(String[] args) {
+
+
+		//hibSession = HibernateUtilConfigured.getSessionFactory().getCurrentSession();
+		//hibSession.beginTransaction();
+		
+		try
+		{ 
+			org.hibernate.tool.hbm2ddl.SchemaExport export = new org.hibernate.tool.hbm2ddl.SchemaExport(HibernateUtilConfigured.getAnnotationConfiguration());//, hibSession.connection());
+			export.setOutputFile("schema-export.sql");
+			export.create(true,false);
+			
+			
+		//	hibSession.flush();
+		//	hibSession.getTransaction().commit();
+	
+		}
+		catch(Exception ex)
+		{ 
+			handleException(ex);
+		}
+
+	}
+
+	public static void handleException(Exception ex){
+		System.out.println("exception occured: " + ex.getMessage());
+		ex.printStackTrace();
+		hibSession.getTransaction().rollback();
+		throw new RuntimeException("conversion failed!");
+	}
+
+}
