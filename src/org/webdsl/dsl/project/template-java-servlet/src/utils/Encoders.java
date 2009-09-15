864a8f80e30a1d9860474110ac0a59695fa0b045
MichelWeststrate <MichelWeststrate>
2009-09-15 15:11:05 +0000

    * optimized encoders * image now supports expression arguments
    svn path=/webdsls/trunk/; revision=3057

--- src/org/webdsl/dsl/project/template-java-servlet/src/utils/Encoders.java
+++ src/org/webdsl/dsl/project/template-java-servlet/src/utils/Encoders.java
@@ -3,34 +3,36 @@ package utils;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
+import java.util.HashMap;
 import javax.faces.convert.ConverterException;
-
+import org.apache.commons.codec.binary.Hex;
 
 public final class Encoders {
+    
+    public static MessageDigest md = null;
+    private static int hits = 0;
+    
     public static String encodeTemplateId(String name, Object[] args,int counter)
     {
+        //mw: append before update, and cache:
+        String d = name + counter;
         try{
-            // Create a Message Digest from a Factory method 
-            MessageDigest md = MessageDigest.getInstance("MD5");
+            hits+=1;
+            // Create a Message Digest from a Factory method
+            if (md == null)
+                md = MessageDigest.getInstance("MD5");
 
             // Create the message
-            byte[] msg = name.getBytes();
-
             // Update the message digest with some more bytes
             // This can be performed multiple times before creating the hash
-            md.update(msg);
+            md.reset();
+            md.update(d.getBytes());
             //for(Object o:args)
             //    md.update(o.toString().getBytes());
-            md.update((""+counter).getBytes());
 
             // Create the digest from the message
-            byte[] messageDigest = md.digest();
-
-            BigInteger number = new BigInteger(1,messageDigest);
-
-            return number.toString(16);
-
+            String s = new String(Hex.encodeHex(md.digest()));
+            return s;
         }
         catch(NoSuchAlgorithmException e)
         {
