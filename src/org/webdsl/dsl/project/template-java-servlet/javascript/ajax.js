1334483dae6a0ef60c5438ea1cf0aa42bbe04209
MichelWeststrate <MichelWeststrate>
2009-09-21 12:38:01 +0000

    use UTF-8 encoding consequently, to support strange tokens
    svn path=/webdsls/trunk/; revision=3089

--- src/org/webdsl/dsl/project/template-java-servlet/javascript/ajax.js
+++ src/org/webdsl/dsl/project/template-java-servlet/javascript/ajax.js
@@ -36,7 +36,10 @@ function formToJSON(formObj) {
 
 function encodePost(string)
 {
-  return escape(encodeURI(string).replace(/%20/g,"+"));  //stupid replace because of difference between urlencode, en post encoding (see http://www.devpro.it/examples/php_js_escaping.php), 
+    if (string == undefined) 
+      return "";
+    return escape(Utf8.encode(string).replace(/\+/g,"%2B"));
+ // return escape(encodeURI(string).replace(/%20/g,"+"));  //stupid replace because of difference between urlencode, en post encoding (see http://www.devpro.it/examples/php_js_escaping.php), 
                                                         //using regexp to replaceAll
 }
 
@@ -355,3 +358,55 @@ function loadCSS(url) {
     e.setAttribute("href", url)
     document.getElementsByTagName("head")[0].appendChild(e);  	
 }
+
+//UTF encoding: http://www.webtoolkit.info/javascript-utf8.html
+var Utf8 = {
+  // public method for url encoding
+  encode : function (string) {
+    string = string.replace(/\r\n/g,"\n");
+    var utftext = "";
+    for (var n = 0; n < string.length; n++) {
+      var c = string.charCodeAt(n);
+      if (c < 128) {
+        utftext += String.fromCharCode(c);
+      }
+      else if((c > 127) && (c < 2048)) {
+        utftext += String.fromCharCode((c >> 6) | 192);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+      else {
+        utftext += String.fromCharCode((c >> 12) | 224);
+        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+    }
+    return utftext;
+  },
+  // public method for url decoding
+  decode : function (utftext) {
+    var string = "";
+    var i = 0;
+    var c = c1 = c2 = 0;
+    while ( i < utftext.length ) {
+      c = utftext.charCodeAt(i);
+      if (c < 128) {
+        string += String.fromCharCode(c);
+        i++;
+      }
+      else if((c > 191) && (c < 224)) {
+        c2 = utftext.charCodeAt(i+1);
+        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+        i += 2;
+      }
+      else {
+        c2 = utftext.charCodeAt(i+1);
+        c3 = utftext.charCodeAt(i+2);
+        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+        i += 3;
+      }
+    }
+    return string;
+  }
+};
+
+function void() {} ;
