1d35c4565b7091e62bb286bbb9c98430c2494f02
Rob Vermaas <rob.vermaas@gmail.com>
2009-09-23 11:17:31 +0000

    * changed from configure time @prefix@ to calculated prefix to allow relocation of statically linked webdsl
    svn path=/webdsls/trunk/; revision=3101

--- src/org/webdsl/dsl/project/testnewproject.sh
+++ src/org/webdsl/dsl/project/testnewproject.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+export
 echo "Testing 'webdsl new' application"
 cd new_project
-bash ../webdsl test hello > /dev/null 2> /dev/null
+bash /opt/webdsl/bin/webdsl test hello > /dev/null 2> /dev/null
