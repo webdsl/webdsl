53cd023571c03b913449ddac56fe7dbc0cc5a513
Danny Groenewegen <dgroenewegen@gmail.com>
2010-01-18 13:42:25 +0000

    webdsl script converted to Ant tasks
    changed application.ini to java properties file format (old style application.ini files are automatically converted)
    
    svn path=/webdsls/trunk/; revision=3500

--- src/org/webdsl/dsl/project/run-ant-webdsl-build.sh
+++ src/org/webdsl/dsl/project/run-ant-webdsl-build.sh
@@ -1,1 +1,1 @@
-ant -f webdsl-build.xml -Dtemplatedir=/Users/dan/workspace/webdsl/src/org/webdsl/dsl/project -Dcurrentdir=. $1
+ant -f webdsl-build.xml -Dtemplatedir=/Users/dan/workspace/webdsl/src/org/webdsl/dsl/project -Dcurrentdir=. -Dwebdslexec=/usr/local/bin/webdslc $1
