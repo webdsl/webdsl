12b21fe9bb82219276022a8e118ceb9dcc9732dc
Danny Groenewegen <dgroenewegen@gmail.com>
2010-01-23 09:55:42 +0000

    batch file for using webdsl-java on windows
    svn path=/webdsls/trunk/; revision=3522

---
+++ src/org/webdsl/dsl/project/webdsl.bat
@@ -0,0 +1,5 @@
+set BINPREFIX=%~dp0
+set PREFIX=%BINPREFIX%..\
+set PWD=%CD%
+set SIGILSTAR=%*
+ant -f "%PREFIX%share\webdsl\webdsl-build.xml" -Dtemplatedir="%PREFIX%share\webdsl" -Dcurrentdir="%PWD%" -Dwebdslexec="java -ss4m -cp %BINPREFIX%webdsl.jar org.webdsl.webdslc.Main" -Dbuildoptions="%SIGILSTAR%" command
