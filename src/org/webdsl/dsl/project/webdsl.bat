60e0d85ff37ef50c052bd9e137ea0b3da3441754
Danny Groenewegen <dgroenewegen@gmail.com>
2010-02-09 15:39:28 +0000

    svn path=/webdsls/trunk/; revision=3607

--- src/org/webdsl/dsl/project/webdsl.bat
+++ src/org/webdsl/dsl/project/webdsl.bat
@@ -2,4 +2,4 @@ set BINPREFIX=%~dp0
 set PREFIX=%BINPREFIX%..\
 set PWD=%CD%
 set SIGILSTAR=%*
-ant -f "%PREFIX%share\webdsl\webdsl-build.xml" -Dtemplatedir="%PREFIX%share\webdsl" -Dcurrentdir="%PWD%" -Dwebdslexec="java -ss4m -cp %BINPREFIX%webdsl.jar org.webdsl.webdslc.Main" -Dbuildoptions="%SIGILSTAR%" command
+ant -f "%PREFIX%share\webdsl\webdsl-build.xml" -Dtemplatedir="%PREFIX%share\webdsl" -Dcurrentdir="%PWD%" -Dwebdsl-java-cp="%BINPREFIX%webdsl.jar" -Dwebdslexec="java -ss4m -cp %BINPREFIX%webdsl.jar org.webdsl.webdslc.Main" -Dbuildoptions="%SIGILSTAR%" command
