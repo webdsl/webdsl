ad99b38ef3f216543f82b41c21c07efb867eab99
Danny Groenewegen <dgroenewegen@gmail.com>
2010-03-26 12:36:55 +0000

    prevent java.lang.ArrayIndexOutOfBoundsException at sun.security.provider.DigestBase.engineUpdate(DigestBase.java:110)
    svn path=/webdsls/trunk/; revision=3779

--- src/org/webdsl/dsl/project/template-java-servlet/src-template/utils/Encoders.java
+++ src/org/webdsl/dsl/project/template-java-servlet/src-template/utils/Encoders.java
@@ -2,41 +2,29 @@ package utils;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import javax.faces.convert.ConverterException;
 import org.apache.commons.codec.binary.Hex;
 
 public final class Encoders {
     
-    public static MessageDigest md = null;
-    private static int hits = 0;
-    
     public static String encodeTemplateId(String name, String argsrep, String templateContext)
     {
         //mw: append before update, and cache:
         String d = name + templateContext;
-        try{
-            hits+=1;
-            // Create a Message Digest from a Factory method
-            if (md == null)
-                md = MessageDigest.getInstance("MD5");
+       
+        // get a Message Digest from the threadlocal page object, that way, only one MD per thread is used (MD is not thread-safe)
+        MessageDigest md = utils.ThreadLocalPage.get().getMessageDigest();
 
-            // Create the message
-            // Update the message digest with some more bytes
-            // This can be performed multiple times before creating the hash
-            md.reset();
-            md.update(d.getBytes());
-            md.update(argsrep.getBytes());
+        // Create the message
+        // Update the message digest with some more bytes
+        // This can be performed multiple times before creating the hash
+        md.reset();
+        md.update(d.getBytes());
+        md.update(argsrep.getBytes());
 
-            // Create the digest from the message
-            String s = new String(Hex.encodeHex(md.digest()));
-            return s;
-        }
-        catch(NoSuchAlgorithmException e)
-        {
-            System.out.println("MD5 not available: "+e.getMessage());
-            return null;
-        }
+        // Create the digest from the message
+        String s = new String(Hex.encodeHex(md.digest()));
+        return s;
     }
 }
