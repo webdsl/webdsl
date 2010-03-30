07a7c9c1ba3da823cd2139ac283fa6b32c2fe740
Danny Groenewegen <dgroenewegen@gmail.com>
2010-03-30 14:14:13 +0000

    fix test
    svn path=/webdsls/trunk/; revision=3795

--- test/succeed/nativejava/SayHello.java
+++ test/succeed/nativejava/SayHello.java
@@ -1,13 +1,13 @@
 package nativejava;
 
-import utils.PageServlet;
+import utils.AbstractPageServlet;
 
     public class SayHello {
-      public static String sayHello(PageServlet p, String to) {
+      public static String sayHello(AbstractPageServlet p, String to) {
         return "Hello " + to;
       }
       
-      public static String sayHello(PageServlet p, String from, String to) {
+      public static String sayHello(AbstractPageServlet p, String from, String to) {
         return "Hello from " + from + " to " + to;
       }
     }
