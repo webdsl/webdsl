42da1352b9aa63b930167fbc0ec68a483b08eed0
Danny Groenewegen <dgroenewegen@gmail.com>
2011-02-03 16:40:38 +0000

    new input and output templates for Date, DateTime, and Time: - uses JQuery instead of Dojo - defined in WebDSL templates
    svn path=/webdsls/trunk/; revision=4389

--- java-runtime/src/org/webdsl/lang/ReflectionProperty.java
+++ java-runtime/src/org/webdsl/lang/ReflectionProperty.java
@@ -2,14 +2,20 @@ package org.webdsl.lang;
 
 public class ReflectionProperty{
 
-    public ReflectionProperty(String name, boolean notnull){
+    public ReflectionProperty(String name, boolean notnull, String format){
         this.name = name;
         this.hasNotNullAnnotation = notnull;
+        this.formatAnnotation = format;
     }
     
     private String name;
     public String getName(){
       return name;      
+    }
+    
+    private String formatAnnotation;
+    public String getFormatAnnotation(){
+        return formatAnnotation;
     }
     
     private boolean hasNotNullAnnotation = false;
