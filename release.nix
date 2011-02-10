7394026544d63dce5d707dd925eb99c7b20accd5
Rob Vermaas <rob.vermaas@gmail.com>
2011-02-10 13:18:42 +0000

    no ulimit on cygwin
    svn path=/webdsls/trunk/; revision=4415

--- release.nix
+++ release.nix
@@ -62,7 +62,7 @@ let
         configureFlags = "--enable-web-check=no";
         doCheck = if stdenv.isLinux || stdenv.isDarwin then true else false;
         phases = "unpackPhase patchPhase configurePhase buildPhase installPhase checkPhase fixupPhase distPhase finalPhase";
-        preConfigure = ''
+        preConfigure = lib.optionalString (stdenv.system != "i686-cygwin") ''
           ulimit -s ${if stdenv.isDarwin then "64000" else "unlimited"}
         '';
       };
