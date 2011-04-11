ab4a8bb9e1036ca2aee1c5b3e6608c25ec8e979a
Nathan Bruning <nathan.bruning@gmail.com>
2011-04-11 17:29:45 +0000

    Revert r4595; should go in the sepcomp branch, not the trunk
    svn path=/webdsls/trunk/; revision=4599

--- java-runtime/src/org/webdsl/tools/strategoxt/Environment.java
+++ java-runtime/src/org/webdsl/tools/strategoxt/Environment.java
@@ -3,14 +3,14 @@ package org.webdsl.tools.strategoxt;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.spoofax.interpreter.adapter.aterm.WrappedATermFactory;
 import org.spoofax.interpreter.core.InterpreterException;
-import org.spoofax.jsglr.client.Asfix2TreeBuilder;
-import org.spoofax.jsglr.client.InvalidParseTableException;
-import org.spoofax.jsglr.client.ParseTable;
-import org.spoofax.jsglr.io.ParseTableManager;
-import org.spoofax.jsglr.client.SGLR;
+import org.spoofax.jsglr.InvalidParseTableException;
+import org.spoofax.jsglr.ParseTable;
+import org.spoofax.jsglr.ParseTableManager;
+import org.spoofax.jsglr.SGLR;
 import org.strategoxt.HybridInterpreter;
-import org.spoofax.terms.TermFactory;
+import org.strategoxt.lang.terms.TermFactory;
 
 /**
  * Environment class that maintains shared objects.
@@ -19,15 +19,21 @@ import org.spoofax.terms.TermFactory;
  */
 public final class Environment {	
 	private final static TermFactory factory = new TermFactory();
-		
-	private final static ParseTableManager parseTableManager = new ParseTableManager(factory);
+	
+	private final static WrappedATermFactory wrappedFactory = new WrappedATermFactory();
+	
+	private final static ParseTableManager parseTableManager = new ParseTableManager(wrappedFactory.getFactory());
 	
 	public static TermFactory getTermFactory() {
 		return factory;
 	}
 	
+	public static WrappedATermFactory getWrappedTermFactory() {
+		return wrappedFactory;
+	}
+	
 	public static SGLR createSGLR(ParseTable parseTable) {
-		return new SGLR(new Asfix2TreeBuilder(factory), parseTable);
+		return new SGLR(wrappedFactory.getFactory(), parseTable);
 	}
 
 	public static HybridInterpreter createInterpreter() throws IOException, InterpreterException {
