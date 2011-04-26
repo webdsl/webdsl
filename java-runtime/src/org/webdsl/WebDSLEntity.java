03712363d04af57446f1636cc5124c518e152aad
Danny Groenewegen <dgroenewegen@gmail.com>
2011-04-26 12:41:49 +0000

    fixed issue related to 'all' method and inheritance
    svn path=/webdsls/trunk/; revision=4618

--- java-runtime/src/org/webdsl/WebDSLEntity.java
+++ java-runtime/src/org/webdsl/WebDSLEntity.java
@@ -3,13 +3,7 @@ package org.webdsl;
 import java.io.Serializable;
 import java.util.List;
 
-/*
- * The T type argument is a workaround to allow return types of the implementation
- * of functions like all_() to be more specific than List<WebDSLEntity>.
- */
-
-@SuppressWarnings("rawtypes")
-public interface WebDSLEntity<T extends WebDSLEntity> extends Serializable {
+public interface WebDSLEntity extends Serializable {
     boolean isInstance(Class<?> c);
     public boolean instanceOf(String s);
     public Object getId();
@@ -19,5 +13,5 @@ public interface WebDSLEntity<T extends WebDSLEntity> extends Serializable {
     public String getName();
     public void validateSave();
     public boolean isChanged();
-    public List<T> all_();
+    public List<?> all_();
 }
