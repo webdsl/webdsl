75d6d0ad5dde1c0c7d93056462394a67904e09ae
Chris Gersen <chris.theblackshadow@gmail.com>
2011-05-24 13:47:45 +0000

    Global getter functions returning a list of entities can now receive query optimization info from the caller, when it is called by a for loop in a template. This extra information is used when the getter uses a forall loop to build the return list.
    svn path=/webdsls/branch/query-optimization/; revision=4683

---
+++ java-runtime/src/utils/QueryOptimization.java
@@ -0,0 +1,30 @@
+package utils;
+
+public class QueryOptimization {
+	public static org.hibernate.Criteria addQueryOptimization(org.hibernate.Criteria criteria, Object joinstr, Object criterion) {
+		//System.out.println("addQueryOptimization");
+		//System.out.println("joinstr: " + joinstr);
+		//System.out.println("criterion: " + criterion);
+		org.hibernate.Criteria ret = criteria;
+		String[] joins = ((String)joinstr).split("\\|");
+		//System.out.println("joins: " + joins.length);
+		for(int i = 1; i < joins.length; i++) {
+			//System.out.println("join: " + joins[i]);
+			String[] varList = joins[i].split("\\.");
+			if(varList.length < 1) continue;
+			String joinProp = varList[varList.length - 1];
+			String assaciation = joinProp;
+			if(varList.length > 1) { 
+				assaciation = "_" + joins[i].substring(0, joins[i].length() - joinProp.length() - 1).replace(".", "_") + "." + joinProp;
+			}
+			String alias = "_" + joins[i].replace(".", "_");
+			//System.out.println("assaciation: " + assaciation + ", alias: " + alias);
+			ret = ret.createAlias(assaciation, alias, org.hibernate.criterion.CriteriaSpecification.LEFT_JOIN);
+		}
+		if(criterion != null) {
+			//System.out.println("add criterion");
+			ret = ret.add((org.hibernate.criterion.Criterion)criterion);
+		}
+		return ret;
+	}
+}
