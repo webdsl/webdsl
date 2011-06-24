4d1b8cce1f8c7ee8d0105bc000f388e57a078eb8
Danny Groenewegen <dgroenewegen@gmail.com>
2011-06-24 11:24:20 +0000

    fix 'too many open files' issue in watchdog script. added restart script for researchr tomcat
    svn path=/webdsls/trunk/; revision=4736

--- utils/restarttomcat.sh
+++ utils/restarttomcat.sh
@@ -1,7 +1,7 @@
+initctl stop tomcat
 kill -3 `ps axU tomcat | grep /data/tomcat/temp | grep  org.apache.catalina.startup.Bootstrap | awk '{print $1}'`
 kill -9 `ps axU tomcat | grep /data/tomcat/temp | grep  org.apache.catalina.startup.Bootstrap | awk '{print $1}'`
 NOW=$(date +"%y-%m-%d-%T")
 LOGFILE="log-$NOW.log"
 tail -n 10000 /data/tomcat/logs/catalina.out > $LOGFILE
-initctl stop tomcat
 initctl start tomcat
