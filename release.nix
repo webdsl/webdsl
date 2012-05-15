e268d8483354e2e6e3283f34f5100fa93fdf76a8
Chris Melman <chrismelman@hotmail.com>
2012-05-15 11:31:32 +0000

    another try to fix the release
    svn path=/webdsls/trunk/; revision=5026

--- release.nix
+++ release.nix
@@ -139,8 +139,8 @@ let
         sed s/@@webdsl@@/${buildJava}/ webdsl.editor/import.webdsl.from-install-dir.properties > webdsl.editor/import.webdsl.properties
       '';
     };
+    in editor
 
   };
 
-  
 in jobs
