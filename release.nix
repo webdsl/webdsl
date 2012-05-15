56f8330de7f8ec4126f4f4e31083cbcf237c3cdd
Chris Melman <chrismelman@hotmail.com>
2012-05-15 15:15:05 +0000

    in with pkgs; instead of normal in
    svn path=/webdsls/trunk/; revision=5037

--- release.nix
+++ release.nix
@@ -127,7 +127,7 @@ let
 	let
   	pkgs = import nixpkgs { system = "x86_64-linux"; };
 	eclipseFun = (import "${hydraConfig}/eclipse.nix") pkgs ; 
-	in 
+	in with pkgs;
 	import "${hydraConfig}/spoofax-fun.nix" {
       		inherit pkgs;
       		name = "webdsl";
