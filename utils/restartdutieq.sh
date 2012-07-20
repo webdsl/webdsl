266379b19d2cfa76c808922046290c3093898d27
Elmer van Chastelet <evanchastelet@gmail.com>
2012-07-20 14:20:56 +0000

    changed to use initctl instead of catalina.sh start/stop
    svn path=/webdsls/trunk/; revision=5266

--- utils/restartdutieq.sh
+++ utils/restartdutieq.sh
@@ -1,8 +1,8 @@
-/var/tomcat/bin/catalina.sh stop
+initctl stop tomcat
 kill -3 `ps axU tomcat | grep /var/tomcat/temp | grep  org.apache.catalina.startup.Bootstrap | awk '{print $1}'`
 kill -9 `ps axU tomcat | grep /var/tomcat/temp | grep  org.apache.catalina.startup.Bootstrap | awk '{print $1}'`
 NOW=$(date +"%y-%m-%d-%T")
 LOGFILE="log-researchr-$NOW.log"
 tail -n 10000 /var/tomcat/logs/catalina.out > $LOGFILE
 export CATALINA_OPTS="-Xms350m -Xss8m -Xmx8G -Djava.security.egd=file:/dev/./urandom -XX:MaxPermSize=512M -XX:PermSize=512M -XX:-UseGCOverheadLimit -XX:+UseCompressedOops"
-/var/tomcat/bin/catalina.sh start
+initctl start tomcat
