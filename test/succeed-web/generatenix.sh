5c8d3e40bc4f9d75ad99560d6225fdb29ed94cda
Chris Melman <chrismelman@hotmail.com>
2012-10-08 13:56:36 +0000

    generatescript plus first version for webtests
    svn path=/webdsls/trunk/; revision=5416

---
+++ test/succeed-web/generatenix.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+echo "{ nixpkgs ? ../../nixpkgs
+, webdsl ? {outPath = builtins.storePath /nix/store/fy8d3qkfhk3k1y10jpdx09q4w43bnh0a-webdsl-9.7pre4063;}
+, nixos ? ../../nixos
+}:
+let
+  pkgs = import nixpkgs { system = \"i686-linux\"; };
+  build = appname :
+   with import \"${nixos}/lib/testing.nix\" {system = \"i686-linux\";} ;
+      runInMachineWithX {
+        require = [ ./machine.nix ];
+        drv = pkgs.stdenv.mkDerivation {
+          name = \"webdsl-check\";
+          buildInputs = [pkgs.apacheAntOpenJDK pkgs.oraclejdk pkgs.firefox15Pkgs.firefox webdsl];
+          buildCommand = \'\'
+            ensureDir $out
+            cp -R ${webdsl}/share/webdsl/webdsl-check/test/succeed-web/ succeed-web/
+	
+            cd succeed-web
+            TOPDIR=`pwd`
+            FAILED=\"\"
+            export DISPLAY=:0.0
+            header \"Running ${appname}\"
+            result=\"\"
+            cd $TOPDIR/`dirname ./${appname}`
+            FILE=`basename ./${appname} .app`
+
+            echo \"Executing 'webdsl test-web $FILE\"
+            webdsl test-web $FILE 2>&1 || export FAILED="1"
+            stopNest
+            if test -z \"$FAILED\"; then
+              exit 0
+            else
+              exit 1
+            fi
+          \'\';
+        };
+      };
+  
+  list = [" > webtests.nix;
+function containsElement () {
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+excludes=( 	"./manual/ajax-form-validation/templates/templates.app"
+		"./manual/tutorial-splash/v1/data.app"
+		"./manual/tutorial-splash/v1/rootpage.app"
+		"./manual/tutorial-splash/v2/data.app"
+		"./manual/tutorial-splash/v2/lib.app"
+		"./manual/tutorial-splash/v2/rootpage.app"
+		"./manual/tutorial-splash/v2/ui.app"
+		"./manual/tutorial-splash/v3/data.app"
+		"./manual/tutorial-splash/v3/invite.app"
+		"./manual/tutorial-splash/v3/lib.app"
+		"./manual/tutorial-splash/v3/rootpage.app"
+		"./manual/tutorial-splash/v3/ui.app"
+		"./manual/tutorial-splash/v4/ac.app"
+		"./manual/tutorial-splash/v4/data.app"
+		"./manual/tutorial-splash/v4/invite.app"
+		"./manual/tutorial-splash/v4/lib.app"
+		"./manual/tutorial-splash/v4/rootpage.app"
+		"./manual/tutorial-splash/v4/ui.app")
+
+ for app in $(find -name "*.app") 
+do 
+  if (!(containsElement "$app" ${excludes[@]})); then
+    echo "	\"${app#\.\/}\"" >> webtests.nix;
+  fi
+done
+echo "
+  ];
+  
+  jobs = pkgs.lib.listToAttrs (map (f: pkgs.lib.nameValuePair (pkgs.lib.replaceChars [\"/\"] [\"_\"] f) (build f)) list);
+
+in jobs" >> webtests.nix;
---
+++ test/succeed-web/webtests.nix
@@ -0,0 +1,210 @@
+{ nixpkgs ? ../../nixpkgs
+, webdsl ? {outPath = builtins.storePath /nix/store/fy8d3qkfhk3k1y10jpdx09q4w43bnh0a-webdsl-9.7pre4063;}
+, nixos ? ../../nixos
+}:
+let
+  pkgs = import nixpkgs { system = "i686-linux"; };
+  build = appname :
+   with import "/lib/testing.nix" {system = "i686-linux";} ;
+      runInMachineWithX {
+        require = [ ./machine.nix ];
+        drv = pkgs.stdenv.mkDerivation {
+          name = "webdsl-check";
+          buildInputs = [pkgs.apacheAntOpenJDK pkgs.oraclejdk pkgs.firefox15Pkgs.firefox webdsl];
+          buildCommand = \'\'
+            ensureDir 
+            cp -R /share/webdsl/webdsl-check/test/succeed-web/ succeed-web/
+	
+            cd succeed-web
+            TOPDIR=/home/chrismelman/workspaces/webdsl-build/webdsl/test/succeed-web
+            FAILED=""
+            export DISPLAY=:0.0
+            header "Running "
+            result=""
+            cd /.
+            FILE=.
+
+            echo "Executing 'webdsl test-web "
+            webdsl test-web  2>&1 || export FAILED=1
+            stopNest
+            if test -z ""; then
+              exit 0
+            else
+              exit 1
+            fi
+          \'\';
+        };
+      };
+  
+  list = [
+	"override-page.app"
+	"input-version-increase.app"
+	".servletapp/src-webdsl-template/built-in.app"
+	"https.app"
+	"ajax-replace-form.app"
+	"bidirectional-onetoone.app"
+	"name-property.app"
+	"placeholder-replace-generated-id.app"
+	"ref-arg-classes.app"
+	"ref-arg-ajax-pass-on.app"
+	"template-redef-elements.app"
+	"types/file.app"
+	"types/string-types-ajax-validate.app"
+	"types/select-ajax-validate.app"
+	"types/set.app"
+	"types/wikitext.app"
+	"types/secret.app"
+	"types/float.app"
+	"types/url.app"
+	"types/submit.app"
+	"types/entity.app"
+	"types/bool-ajax-validate.app"
+	"types/local-var.app"
+	"types/long.app"
+	"types/label.app"
+	"types/email.app"
+	"types/inline.app"
+	"types/validate-built-in-template.app"
+	"types/string.app"
+	"types/sdfinput.app"
+	"types/input.app"
+	"types/long-ajax-validate.app"
+	"types/int.app"
+	"types/list-input-multiple-bug.app"
+	"types/int-ajax-validate.app"
+	"types/bool.app"
+	"types/text.app"
+	"types/list-input-allowed-subclass-bug.app"
+	"types/checkbox-set.app"
+	"types/float-ajax-validate.app"
+	"types/inputajax.app"
+	"types/input-reference-property.app"
+	"js-event.app"
+	"validate-false-and-flush.app"
+	"entity-property-defaults.app"
+	"ac/submithiding.app"
+	"ac/generated-login.app"
+	"ac/template-rule-copy.app"
+	"ac/acdeniedpage-redefinition.app"
+	"ac/override-generated-login.app"
+	"ac/defined-logout-page.app"
+	"ac/renaming-in-rule-issue.app"
+	"ac/policy.app"
+	"ac/pointcut.app"
+	"ac/logincookie.app"
+	"ac/template-rule-copy-page.app"
+	"ac/template-rule-copy-ajax.app"
+	"ac/templaterule.app"
+	"ac/elements-ac-inherit.app"
+	"ac/templateactionrule.app"
+	"span.app"
+	"data-validation/input-bool-prop.app"
+	"data-validation/form-check-2.app"
+	"data-validation/default-messages.app"
+	"data-validation/withstylesheet/ajax-render.app"
+	"data-validation/action-validate-prevent-redirect.app"
+	"data-validation/input-data-invariant-in-page.app"
+	"data-validation/validate-ajax-replace.app"
+	"data-validation/nullpointer.app"
+	"data-validation/input-without-label.app"
+	"data-validation/dv-ajax-button-ajax-form.app"
+	"data-validation/input-data-invariant-in-action.app"
+	"data-validation/ajax-replace-flush.app"
+	"data-validation/input-validate-order.app"
+	"data-validation/validate-ajax-submit.app"
+	"data-validation/error-message-templates-override.app"
+	"data-validation/dv-ajax-button-regular-form.app"
+	"data-validation/validate-in-elements.app"
+	"data-validation/dv-regular-button-regular-form.app"
+	"data-validation/custom-messages.app"
+	"data-validation/form-check.app"
+	"data-validation/dv-regular-button-ajax-form.app"
+	"data-validation/validate-template-call.app"
+	"ref-arg-of-ref-prop.app"
+	"section-nesting.app"
+	"entityversion.app"
+	"ajax-action-return-relocate-goto.app"
+	"ref-arg-prop.app"
+	"template-redef-with-vars.app"
+	"render-template-function.app"
+	"navigate-null-arg.app"
+	"request-log-entry.app"
+	"type-define-date-format.app"
+	"withtest4.app"
+	"radio.app"
+	"action-page-sig-conflict.app"
+	"requestPage.app"
+	"action-without-form.app"
+	"local-template-var-issue.app"
+	"logsql-ac.app"
+	"invoke-broken-page-first.app"
+	"ajax-url-encoding.app"
+	"elements-empty.app"
+	"ajax-button-form-submit.app"
+	"withtest3.app"
+	"listasparam.app"
+	"ref-arg-ajax-ac.app"
+	"ref-arg-multiple-dereferences.app"
+	"for-loop-action.app"
+	"query-extraction.app"
+	"collection-of-collection.app"
+	"type-define.app"
+	"with-vars.app"
+	"with-refs.app"
+	"local-template-redef.app"
+	"output-collection.app"
+	"withtest2.app"
+	"supercalls.app"
+	"getTypeString.app"
+	"render-template-function-local-redef.app"
+	"elements-multiple-2.app"
+	"withtest1.app"
+	"ref-arg-ajax.app"
+	"ref-arg-page.app"
+	"local-template-redef-ac.app"
+	"override-template.app"
+	"email-tc.app"
+	"ref-arg.app"
+	"securitycontext-property-ac-rule-arg-conflict.app"
+	"ref-arg-collection.app"
+	"search/advanced-search.app"
+	"search/search-namespaces.app"
+	"search/simple-search.app"
+	"search/embedded-search.app"
+	"email-tc-ac.app"
+	"encoding-quotes.app"
+	"logsql.app"
+	"validate-false-and-flush-arg.app"
+	"actionclosure.app"
+	"action-with-form.app"
+	"with.app"
+	"logsql-ac-default.app"
+	"template-redef.app"
+	"built-in-html-wrappers.app"
+	"ref-arg-global.app"
+	"request-log-entry-ac.app"
+	"validate-false-and-query.app"
+	"placeholder-attribute.app"
+	"placeholder-ajax-call.app"
+	"rendertemplate-elements.app"
+	"input-null-string.app"
+	"label-with-columns.app"
+	"email-empty.app"
+	"mimetype.app"
+	"elements-multiple.app"
+	"nested-template-ref-arg.app"
+	"elements.app"
+	"navigate-null.app"
+	"manual/ajax-form-validation/ajaxvalidation.app"
+	"manual/tutorial-splash/v3/v3.app"
+	"manual/tutorial-splash/v1/v1.app"
+	"manual/tutorial-splash/v2/v2.app"
+	"manual/tutorial-splash/v4/v4.app"
+	"manual/acpage.app"
+	"include.app"
+
+  ];
+  
+  jobs = pkgs.lib.listToAttrs (map (f: pkgs.lib.nameValuePair (pkgs.lib.replaceChars ["/"] ["_"] f) (build f)) list);
+
+in jobs
