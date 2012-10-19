daaf2c65bb26c7f6885f04c0b195cffdc3afde16
Chris Melman <chrismelman@hotmail.com>
2012-10-19 14:03:43 +0000

    apperently didn't works as expected
    svn path=/webdsls/trunk/; revision=5446

--- test/generatenix.sh
+++ test/generatenix.sh
@@ -59,7 +59,7 @@ excludes=( 	"./manual/ajax-form-validation/templates/templates.app"
 		"./manual/tutorial-splash/v4/rootpage.app"
 		"./manual/tutorial-splash/v4/ui.app")
 
- for app in ("./ref-arg.app")
+ for app in "./ref-arg.app"
 do 
   if (!(containsElement "$app" ${excludes[@]})); then
     echo "	\"${app#\.\/}\"" >> webtests.nix;
