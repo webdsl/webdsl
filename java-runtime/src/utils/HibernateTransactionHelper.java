db4e09f120b2a20e2a81a01a90abe21430c718db
Chris Melman <chrismelman@hotmail.com>
2012-11-14 14:53:54 +0000

    looks like closing and opening created a new session which could deliver problems/ flush needs to be on otherwise validate doesn't trigger
    svn path=/webdsls/trunk/; revision=5499

--- java-runtime/src/utils/HibernateTransactionHelper.java
+++ java-runtime/src/utils/HibernateTransactionHelper.java
@@ -13,7 +13,7 @@ public class HibernateTransactionHelper {
 	    List<utils.ValidationException> exceptions = new java.util.LinkedList<utils.ValidationException>();		
 	    Session hibSession = HibernateUtil.getCurrentSession();
 		Transaction existingTransaction = hibSession.getTransaction();
-//		hibSession.flush();
+		hibSession.flush();
 		boolean isValid = true;
 		try{
 			ThreadLocalPage.get().validateEntitiesAfterAction();
@@ -41,7 +41,7 @@ public class HibernateTransactionHelper {
 	        ex.printStackTrace();
 	        existingTransaction.rollback();
 		} finally {
-			HibernateUtil.getCurrentSession().close();
+//			HibernateUtil.getCurrentSession().close();
 			Session newSession = ThreadLocalPage.get().openNewTransactionThroughGetCurrentSession();
 			newSession.setFlushMode(FlushMode.COMMIT);
 			ThreadLocalPage.get().cleareEntitiesValidatedAfterAction();
