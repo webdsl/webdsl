9412fd2fe04ac3a06d233bb69301a9750e42f2c4
Danny Groenewegen <dgroenewegen@gmail.com>
2014-01-24 10:33:14 +0000

    replace apacheAntOpenJDK with ant in Nix build files, because of change in nixpkgs
    remove obsolete machine.nix file
    
    svn path=/webdsls/trunk/; revision=5870

--- test/succeed-web/machine.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, ... }:
-{
-  virtualisation.memorySize = pkgs.lib.mkOverride 150 3072;
-  virtualisation.diskSize = 2048;
-  environment.systemPackages = [ pkgs.firefox pkgs.apacheAntOpenJDK pkgs.oraclejdk pkgs.subversion pkgs.maven3 ];
-  system.copySystemConfiguration = false;
-}
-
