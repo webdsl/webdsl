73bdeeb8f3399b88a16042d64d5601bad4112248
Elmer van Chastelet <evanchastelet@gmail.com>
2014-04-15 14:39:32 +0000

    Fixes issue where inline javascript was not executed when included in ajax response.
    svn path=/webdsls/trunk/; revision=5906

--- src/org/webdsl/dsl/project/template-java-servlet/javascript/ajax.js
+++ src/org/webdsl/dsl/project/template-java-servlet/javascript/ajax.js
@@ -3,9 +3,9 @@ function ajax_post_process(node) {
   //script tags, such as for datepicker init, are not evaluated when inserted with ajax, this is a workaround that explicitly runs the script contents
   var inner = node.innerHTML;
   if(inner.indexOf("<script") >= 0){ // innerHTML can be just a string instead of DOM element, which would break $(inner)
-    var reponse = $(inner);
-    var reponseScript = reponse.find("script");
-    $.each(reponseScript, function(idx, val) { eval(val.text); } );
+    var response = $(inner);
+    var responseScript = response.filter("script").add(response.find("script"));
+    $.each(responseScript, function(idx, val) { eval(val.text); } );
   }
 }
 
