e81675c6711b268f008aed8f8d64cfa4516954f9
Danny Groenewegen <dgroenewegen@gmail.com>
2015-03-18 16:12:57 +0000

    Reduce generated code size by not lifting webdsl functions for object creation expressions, instead constructor hooks and property setters return the entity instance to allow method call chaining (fluent interface).
    svn path=/webdsls/trunk/; revision=6049

--- java-runtime/src/org/webdsl/WebDSLEntity.java
+++ java-runtime/src/org/webdsl/WebDSLEntity.java
@@ -7,14 +7,14 @@ public interface WebDSLEntity extends Serializable {
     boolean isInstance(Class<?> c);
     public boolean instanceOf(String s);
     public UUID getId();
-    public void setId(UUID o);
+    public WebDSLEntity setId(UUID o);
     public String get_WebDslEntityType();
     public java.lang.Integer getVersion(); 
-    public void setVersion(java.lang.Integer i);
+    public WebDSLEntity setVersion(java.lang.Integer i);
     public java.util.Date getCreated();
-    public void setCreated(java.util.Date d);
+    public WebDSLEntity setCreated(java.util.Date d);
     public java.util.Date getModified();
-    public void setModified(java.util.Date d);
+    public WebDSLEntity setModified(java.util.Date d);
     public String getName();
     public void validateSave();
     public boolean isChanged();
