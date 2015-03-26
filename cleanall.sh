f03a7dba49616d42c7b068d825b9e3257d71693a
Elmer van Chastelet <evanchastelet@gmail.com>
2015-03-26 12:37:28 +0000

    Clean up the built-ins and support jars.
    svn path=/webdsls/trunk/; revision=6059

--- cleanall.sh
+++ cleanall.sh
@@ -2,5 +2,8 @@ rm -rf `find . -name '.servletapp'`
 rm -rf `find . -name '.webdsl-parsecache'`
 rm -rf `find . -name '.webdsl-fragment-cache'`
 rm -rf java-runtime/bin/
+rm -rf java-runtime/dist/
+rm -f src/org/webdsl/dsl/project/template-java-servlet/lib/webdsl-built-ins.jar
+rm -f src/org/webdsl/dsl/project/template-java-servlet/lib/webdsl-support.jar
 rm -rf .cache/
 rm -rf src/bin/
