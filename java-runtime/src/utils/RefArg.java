37bf236a593a926da8270c2ed89dd4259739dd42
Danny Groenewegen <dgroenewegen@gmail.com>
2015-05-07 15:53:33 +0000

    move common generated ref arg code to superclass
    svn path=/webdsls/trunk/; revision=6080

--- java-runtime/src/utils/RefArg.java
+++ java-runtime/src/utils/RefArg.java
@@ -1,16 +1,39 @@
 package utils;
 
-public interface RefArg
-{ 
-  public Object set(Object obj);
-  public Object get();
-  //used for page ref arg and ajax template ref arg
-  public void load(String uuid);
-  public String getUrlString();
-  public String get_WebDslEntityType();
-  public org.webdsl.WebDSLEntity getEntity();
-  public void validate(String location, java.util.List<utils.ValidationException> exceptions);
-  public java.util.List<String> getValidationErrors();
-  public java.util.List<Object> getAllowed();
-  public org.webdsl.lang.ReflectionProperty getReflectionProperty();
+public abstract class RefArg{
+	
+	public abstract Object set(Object obj);
+
+	public abstract Object get();
+
+	public abstract java.util.List<Object> getAllowed();
+
+	
+	// the following are only overridden for ref arg referring to an entity property, template variable ref arg uses these defaults
+	
+	public void load(String uuid){}
+
+	public org.webdsl.WebDSLEntity getEntity(){
+		return null;
+	}
+
+	public String get_WebDslEntityType(){
+		return "not supported for this type of reference argument (refers to template variable)";
+	}
+
+	public String getUrlString(){
+		return "not supported for this type of reference argument (refers to template variable)";
+	}
+
+	public void validate(String location, java.util.List<utils.ValidationException> exceptions){}
+
+	public java.util.List<String> getValidationErrors(){
+		return new java.util.ArrayList<String>();
+	}
+
+	@SuppressWarnings("rawtypes")
+	public org.webdsl.lang.ReflectionProperty getReflectionProperty(){
+		return null;
+	}
+	
 }
