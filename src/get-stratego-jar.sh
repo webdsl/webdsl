033e6ad9e48a78454b85e3ce8cd7b94d35cb09e3
Danny Groenewegen <dgroenewegen@gmail.com>
2016-03-01 15:22:26 +0100

    Remove all dependencies on obsolete Stratego/SDF/Aterm Nix packages from build.

--- src/get-stratego-jar.sh
+++ src/get-stratego-jar.sh
@@ -1,6 +1,6 @@
 if [ ! -f share/strategoxt/strategoxt/strategoxt.jar ]; then
-  wget http://artifacts.metaborg.org/service/local/repositories/releases/content/org/metaborg/strategoxt-distrib/1.4.0/strategoxt-distrib-1.4.0-bin.tar
-  tar -xf strategoxt-distrib-1.4.0-bin.tar
+  wget http://artifacts.metaborg.org/service/local/repositories/releases/content/org/metaborg/strategoxt-distrib/1.5.0/strategoxt-distrib-1.5.0-bin.tar
+  tar -xf strategoxt-distrib-1.5.0-bin.tar
 fi
 if [ ! -f strategoxt.jar ]; then
   cp share/strategoxt/strategoxt/strategoxt.jar .
