f6c956183c95f9ac18725166d647bf3c31193cdf
Elmer van Chastelet <E.vanChastelet@tudelft.nl>
2017-07-28 08:37:05 +0200

    Allow long domain extensions in email addresses

--- java-runtime/src/utils/EmailType.java
+++ java-runtime/src/utils/EmailType.java
@@ -10,7 +10,7 @@ public class EmailType {
       - Character . (dot, period, full stop) provided that it is not the first or last character, and provided also that it does not appear two or more times consecutively.
     */
     public static Boolean isValid(String s){
-      if(s != null && s.equals("") || s.matches("^([a-zA-Z0-9!#$%&'*\\+\\-/=\\?\\^_`{|}~\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)")) {
+      if(s != null && s.equals("") || s.matches("^([a-zA-Z0-9!#$%&'*\\+\\-/=\\?\\^_`{|}~\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,20}|[0-9]{1,3})(\\]?)")) {
         return true;
       }
       return false;
