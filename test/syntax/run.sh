b8cfc8462b9d2619cb7b9442fb9a2b3c5ca6f9d9
Danny Groenewegen <dgroenewegen@gmail.com>
2018-09-14 16:23:23 +0200

    Replace calls to native stratego tools in syntax test script with equivalent calls to tools in stratego jar. Generate class files for ast2test tool because they are not in stratego jar.

--- test/syntax/run.sh
+++ test/syntax/run.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 set -x #echo on
 
+NATIVEPATH="`pwd`/../../src/share/strategoxt/macosx"
+STRATEGOXT_JAR="`pwd`/../../src/strategoxt.jar"
+
+STRTOOL="java -Xss8m -cp $STRATEGOXT_JAR run"
+# run.java class invokes HybridInterpreter to load and run a strategy class file
+# https://github.com/metaborg/strategoxt/blob/master/strategoxt/stratego-libraries/java-backend/java/commandline/run.java
+
+PACKSDF="$STRTOOL org.strategoxt.tools.main-pack-sdf"
+SDF2TABLE="$NATIVEPATH/sdf2table"
+PPATERM="$STRTOOL org.strategoxt.stratego-aterm.io-pp-aterm"
+AST2TEXT="java -Xss8m -cp $STRATEGOXT_JAR:ast2text/bin run syntaxtest.io-ast2text"
+
 # run setup.sh once to download JSGLR
 if [ ! -f Jsglr.class ]; then
   echo "Jsglr .class not found - running setup.sh"
@@ -8,16 +20,22 @@ if [ ! -f Jsglr.class ]; then
   ./setup.sh
 fi
 
+# compile ast2text utility class files once
+if [ ! -f ast2text/bin/syntaxtest/ast2text_0_0.class ]; then
+  cd ast2text
+  make
+  cd ..
+fi
 
-pack-sdf -I ../../src/org/webdsl/dsl/syntax -i $1.sdf -o $1.def
-sdf2table -i $1.def -o $1.tbl -m $1 
-       
+$PACKSDF -I ../../src/org/webdsl/dsl/syntax -i $1.sdf -o $1.def
+$SDF2TABLE -i $1.def -o $1.tbl -m $1 
+
 for i in `find . -name "$1*.txt"`; do
   VAR=`basename $i`
   echo $VAR
   #sglri -p $1.tbl -i $VAR -o __test-$VAR
   java -cp spoofax-sunshine.jar Jsglr $1.tbl TestDef $VAR > __test-$VAR
-  cat __test-$VAR | pp-aterm
+  cat __test-$VAR | $PPATERM
 
   #sglri -A -p $1.tbl -i $VAR -o __ignore
   #RET=$?
@@ -27,7 +45,7 @@ for i in `find . -name "$1*.txt"`; do
   #fi
 
   #parse-pp-table -i ../../src/org/webdsl/dsl/syntax/WebDSL-pretty.pp -o __WebDSL-pretty.pp.af
-  ast2text -p ../../src/org/webdsl/dsl/syntax/HQL-pretty.pp -p ../../src/org/webdsl/dsl/syntax/WebDSL-pretty.pp -i __test-$VAR -o __pretty-$VAR
+  $AST2TEXT -p ../../src/org/webdsl/dsl/syntax/HQL-pretty.pp -p ../../src/org/webdsl/dsl/syntax/WebDSL-pretty.pp -i __test-$VAR -o __pretty-$VAR
   cat __pretty-$VAR
 done
 
