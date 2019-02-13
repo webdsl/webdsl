da861268bc44b3b1806f311dbe4a407fa331f93f
Danny Groenewegen <dgroenewegen@gmail.com>
2019-02-13 15:05:05 +0100

    Remove unnecessary exception handling, in particular for outputting string literal or string interpolation with expressions in template. Change exception handling in string interpolation to catch all runtime exceptions for nested expressions.

--- java-runtime/src/utils/RenderUtils.java
+++ java-runtime/src/utils/RenderUtils.java
@@ -4,23 +4,10 @@ import java.io.PrintWriter;
 
 public final class RenderUtils {
 
-	public static final void printPageString(Object toPrint, PrintWriter out, boolean rawoutput){
-		String tmpstring;
-		try {
-			if(org.webdsl.tools.Utils.isNullAutoBox(toPrint)){
-				tmpstring = "";
-			}
-			else{
-				tmpstring = rawoutput ? String.valueOf(toPrint) : utils.HTMLFilter.filter(String.valueOf(toPrint));
-			}
+	public static final void printPageString(Object toPrint, PrintWriter out, boolean rawoutput) {
+		if (toPrint != null) {
+			out.print(rawoutput ? String.valueOf(toPrint) : utils.HTMLFilter.filter(String.valueOf(toPrint)));
 		}
-		catch(NullPointerException npe) {
-			tmpstring = "";
-		}
-		catch(IndexOutOfBoundsException ine){
-			tmpstring = "";
-		}
-		out.print(tmpstring);
 	}
 
 }
