399d98793fe4307a5c3a6f569ccf2c32928e7d8c
Elmer van Chastelet <e.vanchastelet@tudelft.nl>
2021-04-20 12:58:47 +0200

    Add "name" to safe attribute list for anchor tag in WikiText

--- java-runtime/src/org/webdsl/tools/WikiFormatter.java
+++ java-runtime/src/org/webdsl/tools/WikiFormatter.java
@@ -43,7 +43,7 @@ public final class WikiFormatter {
              .addAttributes("td", "align")
              .addAttributes("code", "class")
              .addAttributes("div", "class")
-             .addAttributes("a", "rel")
+             .addAttributes("a", "rel", "name")
              .addAttributes("details", "class")
              .addAttributes("details", "open");
     //allow id's on title tags
